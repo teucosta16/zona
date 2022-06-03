7a958e63ae3d3be143682ee6394684bffd87dbb6
teucosta16 <teu.costa.16@gmail.com>
2022-06-03 16:55:41 -0300

    zon

---
+++ zona.js
@@ -0,0 +1,14 @@
+setTimeout(function() {
+
+    let a = document.getElementById("initi");
+
+    a.style = "display:none"
+
+}, 3000);
+
+setTimeout(function() {
+    let b = document.getElementById("initi_2")
+
+    b.style = "display:block"
+
+}, 3500);
